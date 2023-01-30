e9d944aa87260555eba0081324dc6fbdd3f6055b
Oleg833 <oleg.kovtun447@gmail.com>
2023-01-30 06:52:23 +0200

    Task1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const itemElements = document.querySelectorAll('.item');
+console.log(`Number of categories: ` + itemElements.length);
+const resultArray = [...itemElements]
+  .map(
+    item => `Category: ${item.children[0].textContent} 
+    Elements: ${item.children[1].children.length}`
+  )
+  .join('\n');
+console.log(resultArray);
