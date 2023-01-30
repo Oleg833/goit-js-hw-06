b282084cd0e9dc078e295978d73c088a8673da28
Oleg833 <oleg.kovtun447@gmail.com>
2023-01-30 09:15:52 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const markup = ingredients.map(ingredient => `<li class="item">${ingredient}</li>`).join('\n');
+
+document.querySelector('ul#ingredients').innerHTML = markup;
+
+// Check the console, you'll see a single string with HTML tags
+console.log(markup);
+
+// const lastFragment = new DocumentFragment();
+// for (const item of ingredients) {
+//   const list = document.createElement('li');
+//   list.textContent = item;
+//   list.classList.add('item');
+//   lastFragment.appendChild(list);
+// }
+// document.querySelector('ul#ingredients').append(lastFragment);
