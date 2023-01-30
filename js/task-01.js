7ad85f3e3f44a5e857ab5b00d003f48c7ab0359f
Oleg833 <oleg.kovtun447@gmail.com>
2023-01-30 07:07:30 +0200

    Clear 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,6 @@
 const itemElements = document.querySelectorAll('.item');
 console.log(`Number of categories: ` + itemElements.length);
+
 const resultArray = [...itemElements]
   .map(
     item => `Category: ${item.children[0].textContent} 
